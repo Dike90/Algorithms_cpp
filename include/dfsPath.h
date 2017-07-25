d9278856dfa54f15f427af905f20c4881108dd0d
Dike90 <919055994@qq.com>
2017-07-25 16:27:02 +0800

    add dfsPath

--- include/dfsPath.h
+++ include/dfsPath.h
@@ -1,15 +1,39 @@
 #ifndef __DFSPATH_H__
 #define __DFSPATH_H__
+#include <stack>
 #include "../include/graph.h"
 class DFSPaths{
-    bool marked;
+    bool *marked;
     int *edgeTo;
     const int s;
+    void dfs(Graph &g,int v){
+        marked[v] = true;
+        for (int w : g.getAdj(v))
+            if ( !marked[w]){
+                edgeTo[w] = v;
+                dfs(g,w);
+            }
+    }
 public:
-    DFSPaths(Graph &g,int s){
-        marked = new bool[g.getV()];
-        edgeTo = new int[g.getV()];
-        
+    DFSPaths(Graph &g,int s) : s(s){
+        marked = new bool[g.getV()]();
+        edgeTo = new int[g.getV()]();
+        dfs(g,s);
+    }
+    ~DFSPaths(){
+        delete [] marked;
+        delete [] edgeTo;
+    }
+    bool hasPathTo(int v){
+        return marked[v];
+    }
+    std::stack<int> pathTo(int v){
+        std::stack<int> path;
+        if (!marked[v]) return path;
+        for (int x=v; x != s; x = edgeTo[x])
+            path.push(x);
+        path.push(s);
+        return path;
     }
 };
 
--- src/dfsPath.cpp
+++ src/dfsPath.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
-#include "../include/graph.h"
+#include "../include/dfsPath.h"
 
 using namespace std;
 
 int main(){
     ifstream fin("../algs4-data/tinyCG.txt");
     Graph graph(fin);
-    bool *marked = new bool[graph.getV()];
-    int *edgeTo = new int[graph.getV()];
+    DFSPaths dfspath(graph,0);
+    stack<int> path;
+    path = dfspath.pathTo(4);
+    while(!path.empty()){
+        std::cout<<path.top()<<" ";
+        path.pop();
+    }
+        
+    cout<<endl;
     
 
     cin.get();
